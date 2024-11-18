2d8ec06d23b941a95828aecfab0b16f19838a95f
Lewis Ashcroft <lewis1105@live.com>
2024-11-18 08:08:28 +0000

    Inital Commit/Project Setup

---
+++ src/test/java/com/example/makersbnb/MakersbnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.makersbnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakersbnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
